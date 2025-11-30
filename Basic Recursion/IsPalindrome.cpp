5dd2b0132e1e4a5764f32f5986b77a2c20c07cab
Arthur-071 <arthur7nova@gmail.com>
2025-11-30 16:22:40 +0530

    Check Palindrome

---
+++ Basic Recursion/IsPalindrome.cpp
@@ -0,0 +1,11 @@
+there is a way to do it without using an extra string
+
+bool palindromeCheck(string& s) {
+    int left = 0, right = s.length() - 1;
+    while (left < right) {
+        if (s[left] != s[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+}
